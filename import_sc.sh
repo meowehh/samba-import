fef6a346ea850ead837bd88405d2b625119d9007
meowehh <sv01092013@gmail.com>
2025-06-19 21:24:13 +0700

    Add files via upload

---
+++ import_sc.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+csv_file="/opt/users.csv"
+while IFS=";" read -r firstName lastName role phone ou street zip city 
+country password; do
+	if ["$firstName"=="First Name"]; then
+		continue
+	fi
+	username="${firstName,,}.${lastName..}"
+	sudo samba-tool user add "$username" P@ssw0rd1
+done < "$csv_file"
