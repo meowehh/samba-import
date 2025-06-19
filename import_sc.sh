882f3d3dc10d46424ec972c84e7084b9e2d23e86
meowehh <sv01092013@gmail.com>
2025-06-19 21:37:15 +0700

    Update import_sc.sh

--- import_sc.sh
+++ import_sc.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 csv_file="/opt/users.csv"
-while IFS=";" read -r firstName lastName role phone ou street zip city 
-country password; do
-	if ["$firstName"=="First Name"]; then
-		continue
-	fi
-	username="${firstName,,}.${lastName..}"
-	sudo samba-tool user add "$username" P@ssw0rd1
+while IFS=";" read -r firstName lastName role phone ou street zip
+        if [ "$firstName" == "First Name" ]; then
+                continue
+        fi
+        username="${firstName,,}.${lastName,,}"
+        sudo samba-tool user add "$username" P@ssw0rd1
 done < "$csv_file"
