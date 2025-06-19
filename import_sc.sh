83ba3bd1a6650b371ad5933941ced8b9a0b48e99
meowehh <sv01092013@gmail.com>
2025-06-19 21:43:49 +0700

    Delete import_sc.sh

--- import_sc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-csv_file="/opt/users.csv"
-while IFS=";" read -r firstName lastName role phone ou street zip
-        if [ "$firstName" == "First Name" ]; then
-                continue
-        fi
-        username="${firstName,,}.${lastName,,}"
-        sudo samba-tool user add "$username" P@ssw0rd1
-done < "$csv_file"
